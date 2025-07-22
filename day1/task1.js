7eacf4aea40f68604021a499d7ff55eb9983d362
ShekFarman <shekfarman18@gmail.com>
2025-07-22 19:34:20 +0530

    task1.js

---
+++ day1/task1.js
@@ -0,0 +1,14 @@
+var name = "das";
+console.log("First name :",name);
+
+name = "leo";
+console.log("modified name :",name);
+
+let age = 28;
+console.log("first age:",age);
+
+age = 50;
+console.log("modified age :",age);
+
+const year = 2006;
+console.log("Year :",year);
